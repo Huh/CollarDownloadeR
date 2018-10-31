c300dc026e649ff403a4d9897f97c5b9110f3dfc
Josh Nowak <Joshnwk@gmail.com>
2018-10-30 23:50:23 -0600

    Improved documentation and data types

--- R/build_urls.R
+++ R/build_urls.R
@@ -1,33 +1,49 @@
 #' Functions to build the url required to call the API
 #'
 #' The url is composed of a base that never changes, a collar id, a collar key
-#' and data type at a minimum.  These pieces and the optional parameters
-#' after_data_id and start_date must be put together in a rather particular
-#' order with appropriate punctuation.  This is the role of these functions.
+#' data type and either a start id or date at a minimum.  These pieces must be
+#' put together in a rather particular order with appropriate punctuation.  This
+#' is the role of these functions.
 #'
 #' @param base_url NULL or the url to build from, the default NULL should
 #' suffice in almost all cases
 #' @param collar_id The ID(s) of the collars to query for data
 #' @param collar_key The key(s) of the collars to query for data
-#' @param type The data type, poorly defined at the moment
-#' @param after_data_id Some data types have ID numbers, if you wish to only
-#' download data after some data id then supply it here and the API should only
-#' return data with an ID greater than that supplied.  Must be equal in length
-#' to collar_id and collar_key.  Only one of start_date and
-#' after_data_id may be supplied.
-#' @param start_date A date class vector.  Supplying this parameter will cause
-#' the API to only return data collected after the supplied date.  Must be equal
-#' in length to collar_id and collar_key.  Only one of start_date and
-#' after_data_id may be supplied.
+#' @param type The data type, options include "gps", "act", "mit", "mor", "prx",
+#' "SEP" and "TRAP".  See details.
+#' @param after_data_id All data types have a unique ID maintained by the
+#' manufacturer.  Use this parameter to download data after the supplied data
+#' ID.  Must be equal in length to collar_id and collar_key.  Only one of
+#' start_date and after_data_id may be supplied, but one of the two must be
+#' supplied.
+#' @param start_date A character vector specifying date and time as
+#' DD-MM-YYYYTHH:MM:SS (the T is required, for example 01-01-2018T00:00:00).
+#' Supplying this parameter will cause the API to only return data collected
+#' after the supplied date.  Must be equal in length to collar_id and
+#' collar_key.  Only one of start_date and after_data_id may be supplied, but
+#' one must be supplied.
 #'
 #' @details The functions cdr_build_vec_url and cdr_build_vec_urls differ only
 #' in that cdr_build_vec_url builds a single url from a collection of length 1
-#' arguments while cdr_build_vec_url will function on vectors of inputs to
+#' arguments while cdr_build_vec_urls will function on vectors of inputs to
 #' create numerous urls.  Functionally the user is encouraged to only use the
 #' plural version so that code will function regardles of the length of the
 #' input.
 #'
-#' @return A url as a character string
+#' Data types are defined by the user and only a single data type is returned by
+#' the API for any given call.  The url tells the API which data type to return,
+#' such that passing
+#' \itemize{
+#'   \item "gps" returns location data
+#'   \item "act" returns activity data
+#'   \item "mit" returns implant mortality data
+#'   \item "mor" returns mortality data
+#'   \item "prx" returns proximity data
+#'   \item "SEP" returns separation data
+#'   \item "TRAP" returns trap event data
+#' }
+#'
+#' @return Formatted url(s) as a character string
 #' @export
 #'
 #' @examples
@@ -45,11 +61,13 @@
 #'   type = "act"
 #' )
 cdr_build_vec_url <- function(base_url = NULL,
-                              collar_id = NULL,
-                              collar_key = NULL,
-                              type = c("act", "csq", "mit"),
-                              after_data_id = NULL,
-                              start_date = NULL) {
+                               collar_id = NULL,
+                               collar_key = NULL,
+                               type = c("gps", "act", "mit", "mor", "prx",
+                                 "SEP", "TRAP"),
+                               after_data_id = NULL,
+                               start_date = NULL) {
+
   one_null <- function(x, y) {
     is.null(x) | is.null(y)
   }
@@ -59,6 +77,9 @@ cdr_build_vec_url <- function(base_url = NULL,
   }
 
   assertthat::assert_that(one_null(after_data_id, start_date))
+  assertthat::assert_that(
+      type %in% c("gps", "act", "mit", "mor", "prx", "SEP", "TRAP")
+  )
 
   if (is.null(base_url)) {
     base_url <- "https://wombat.vectronic-wildlife.com:9443"
@@ -81,29 +102,22 @@ cdr_build_vec_url <- function(base_url = NULL,
   }
 
   if (!is.null(start_date)) {
-    assertthat::assert_that(assertthat::is.date(start_date))
-
-    frmt_start_date <- format(start_date, "%d-%m-%Y")
-
-    url <- paste0(url, "&after=", frmt_start_date)
+    url <- paste0(url, "&after=", start_date)
   }
 
   url
 }
 
+#' @rdname cdr_build_vec_url
+#' @export
+
 cdr_build_vec_urls <- function(base_url = NULL,
                                collar_id = NULL,
                                collar_key = NULL,
-                               type = c("act", "csq", "mit"),
+                               type = c("gps", "act", "mit", "mor", "prx",
+                                 "SEP", "TRAP"),
                                after_data_id = NULL,
                                start_date = NULL) {
-  equal_length <- function(x, y) {
-    length(x) == length(y)
-  }
-
-  assertthat::on_failure(equal_length) <- function(call, env) {
-
-  }
 
   purrr::map2_chr(
     collar_id, collar_key,
